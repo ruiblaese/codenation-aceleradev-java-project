4e653fd213c2dd309246a5d859ba59979e980755
Rui Richard Blaese <ruiblaese@gmail.com>
2020-04-21 14:33:19 -0300

    UserRepository

---
+++ src/main/java/com/blaese/error/manager/entity/User.java
@@ -0,0 +1,27 @@
+package com.blaese.error.manager.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 6036815330898209680L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+}
---
+++ src/test/java/com/blaese/error/manager/repository/UserRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.blaese.error.manager.repository;
+
+import com.blaese.error.manager.entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserRepositoryTest {
+
+    private static final String EMAIL = "email@teste.com.br";
+
+    @Autowired
+    UserRepository repository;
+
+    @Before
+    public void setUp(){
+        User user = new User();
+        user.setName("SetUp User");
+        user.setPassword("123456");
+        user.setEmail(EMAIL);
+
+        repository.save(user);
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void testSave(){
+
+        User user2 = new User();
+        user2.setName("Test name");
+        user2.setPassword("123456");
+        user2.setEmail("teste@teste.com.br");
+
+        User response = repository.save(user2);
+
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testFindByEmail(){
+        Optional<User> response = repository.findByEmailEquals(EMAIL);
+
+        assertTrue(response.isPresent());
+        assertEquals(EMAIL,response.get().getEmail());
+
+    }
+
+}
